c5784f44599b24a8504a05d6b456466ad24d79f9
King <King@Kunta>
2015-08-03 23:36:50 +0300

    Added some methods to DBTemplate.

--- SummaryTask4/src/ua/nure/zhabin/SummaryTask4/db/DBTemplate.java
+++ SummaryTask4/src/ua/nure/zhabin/SummaryTask4/db/DBTemplate.java
@@ -35,5 +35,86 @@ public class DBTemplate<E> {
 		}
 		return list;
 	}
+	
+	public E get(Connection connection, String sql, Extractor<E> extractor, int i) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		E object = null;
+		try {
+			ps = connection.prepareStatement(sql);
+			ps.setInt(1, i);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				object = extractor.extract(rs);
+			}
+		} catch (SQLException e) {
+			LOG.error("Cannot obtain object!", e);
+		} finally {
+			DBUtil.close(ps);
+			DBUtil.close(rs);
+		}
+		return object;
+	}
+	
+	public E get(Connection connection, String sql, Extractor<E> extractor, String s) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		E object = null;
+		try {
+			ps = connection.prepareStatement(sql);
+			ps.setString(1, s);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				object = extractor.extract(rs);
+			}
+		} catch (SQLException e) {
+			LOG.error("Cannot obtain object!", e);
+		} finally {
+			DBUtil.close(ps);
+			DBUtil.close(rs);
+		}
+		return object;
+	}
 
+	public List<E> getAllByParameter(Connection connection, Parameter parameter,
+			Extractor<E> extractor) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<E> list = new ArrayList<>();
+		try {
+			ps = connection.prepareStatement(parameter.getSql());
+			for (int i = 0; i < parameter.getParams().size(); i++) {
+				ps.setObject(i + 1, parameter.getParams().get(i));
+			}
+			LOG.info("Complete SQL query: " + ps);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				E object = extractor.extract(rs);
+				list.add(object);
+			}
+		} catch (SQLException e) {
+			LOG.error("Cannot obtain objects!", e);
+		} finally {
+			DBUtil.close(ps);
+			DBUtil.close(rs);
+		}
+		return list;
+	}	
+	
+	public void add(Connection connection, String sql, Object[] arr) {
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(sql);
+			for (int i = 0; i < arr.length; i++) {
+				ps.setObject(i + 1, arr[i]);
+			}
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			LOG.error("Cannot obtain object!", e);
+		} finally {
+			DBUtil.close(ps);
+		}
+	}
+	
+	
 }
